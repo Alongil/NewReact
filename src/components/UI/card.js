68019a385561ffef90224710b6f5f746d9823d63
Alongil <alongiladbp@gmail.com>
2021-06-22 17:58:56 +0300

    First React Project

---
+++ src/components/UI/card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './card.css';
+
+
+function Card(props){
+    console.log(props.children);
+    const classes = 'card ' + props.className;
+    console.log(classes);
+    return <div className={classes}>{props.children}</div>;
+}
+
+export default Card;
